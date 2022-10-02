b24de0cf2dbbbe438d17acaf3e2ac14b1f0752f3
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-02 07:06:00 +0300

    add variable in answer.dart
    bool isCorrectAnswer

--- lib/qa/answer.dart
+++ lib/qa/answer.dart
@@ -2,12 +2,21 @@ import 'package:flutter/cupertino.dart';
 
 class Answer {
   Widget _answer = Text("Emtpy Answer!");
+  bool _isCorrectAnswer = false;
 
-  Answer({required Widget answer}) {
+  Answer({
+    required Widget answer,
+    bool isCorrectAnswer = false,
+  }) {
     _answer = answer;
+    _isCorrectAnswer = isCorrectAnswer;
   }
 
   Widget getAnswer() {
     return _answer;
+  }
+
+  bool isCorrectAnswer() {
+    return _isCorrectAnswer;
   }
 }
