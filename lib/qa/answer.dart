e3819b873b01dc8fc36ac5478ab81e61316033fc
Ramzi Sayegh <sayegh.ramzi@yahoo.com>
2022-10-02 10:54:46 +0300

    add functins in answer.dart

--- lib/qa/answer.dart
+++ lib/qa/answer.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Answer {
   Widget _answer = Text("Emtpy Answer!");
   bool _isCorrectAnswer = false;
+  Color _optionColor = Colors.black;
 
   Answer({
     required Widget answer,
@@ -18,5 +19,34 @@ class Answer {
 
   bool isCorrectAnswer() {
     return _isCorrectAnswer;
+  }
+
+  Color getOptionColor() {
+    return _optionColor;
+  }
+
+  void setOptionColor() {
+    if (this.isCorrectAnswer()) {
+      _optionColor = Colors.green;
+    } else {
+      _optionColor = Colors.red;
+    }
+  }
+}
+
+class AnswerWidget extends StatefulWidget {
+  const AnswerWidget({super.key});
+
+  @override
+  State<AnswerWidget> createState() => _AnswerWidgetState();
+}
+
+class _AnswerWidgetState extends State<AnswerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber,
+      child: Text("AnswerWidget"),
+    );
   }
 }
